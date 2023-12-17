63c99d25a61cbe3285d3381a3126616242c72d6e
Krutik30 <agherakrutik99@gmail.com>
2023-12-17 20:56:23 +0530

    added jwt and now implemet the private routes

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStretegy } from './stretegy/jwt.stretegy';
 
 @Module({
   imports: [JwtModule.register({})],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStretegy],
 })
 export class AuthModule { }
